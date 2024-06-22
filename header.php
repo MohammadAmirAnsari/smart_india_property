e576c8d3232ebec6d8d907b21686853b4a2d989a
Mohammad_Amir <aamiransari92111721@gmail.com>
2024-06-22 19:13:14 +0530

    First Commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Untree.co" />
+    <link rel="shortcut icon" href="assests/images/sip_logo.png" />
+
+    <meta name="description" content="" />
+    <meta name="keywords" content="bootstrap, bootstrap5" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
+
+    <link rel="stylesheet" href="./assests/css/icomoon.css" />
+    <link rel="stylesheet" href="./assests/css/flaticon.css" />
+
+    <link rel="stylesheet" href="./assests/css/tiny-slider.css" />
+    <link rel="stylesheet" href="./assests/css/aos.css" />
+    <link rel="stylesheet" href="./assests/css/style.css" />
+    <link rel="stylesheet" href="./assests/css/custom.css" />
+
+    <title>Smart India Properties</title>
+</head>
